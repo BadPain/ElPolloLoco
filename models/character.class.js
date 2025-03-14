4243698fddd46cf99f1c3c2f92538a0f87da4136
IcePain <143926986+BadPain@users.noreply.github.com>
2025-03-14 22:24:37 +0100

    include gravitation, expanded character (jump,sounds,images, hurt and dead)

--- models/character.class.js
+++ models/character.class.js
@@ -1,10 +1,10 @@
 class Character extends MovableObject {
 
-    y = 280;
+    y = 200;
     speed = 10;
     width = 90;
     height = 150;
-    
+
 
     IMAGES_WALKING = [
         'img/main/2_character_pepe/2_walk/W-21.png',
@@ -14,41 +14,88 @@ class Character extends MovableObject {
         'img/main/2_character_pepe/2_walk/W-25.png',
         'img/main/2_character_pepe/2_walk/W-26.png'
     ];
+
+    IMAGES_JUMPING = [
+        'img/main/2_character_pepe/3_jump/J-31.png',
+        'img/main/2_character_pepe/3_jump/J-32.png',
+        'img/main/2_character_pepe/3_jump/J-33.png',
+        'img/main/2_character_pepe/3_jump/J-34.png',
+        'img/main/2_character_pepe/3_jump/J-35.png',
+        'img/main/2_character_pepe/3_jump/J-36.png',
+        'img/main/2_character_pepe/3_jump/J-37.png',
+        'img/main/2_character_pepe/3_jump/J-38.png',
+        'img/main/2_character_pepe/3_jump/J-39.png'
+    ];
+
+    IMAGES_DEAD = [
+        'img/main/2_character_pepe/5_dead/D-51.png',
+        'img/main/2_character_pepe/5_dead/D-52.png',
+        'img/main/2_character_pepe/5_dead/D-53.png',
+        'img/main/2_character_pepe/5_dead/D-54.png',
+        'img/main/2_character_pepe/5_dead/D-55.png',
+        'img/main/2_character_pepe/5_dead/D-56.png',
+        'img/main/2_character_pepe/5_dead/D-57.png'
+    ];
+
+    IMAGES_HURT = [
+        'img/main/2_character_pepe/4_hurt/H-41.png',
+        'img/main/2_character_pepe/4_hurt/H-42.png',
+        'img/main/2_character_pepe/4_hurt/H-43.png'
+    ];
+
     world;
+    walking_sound = new Audio('audio/walk_new3.mp3');
+    jumping_sound = new Audio('audio/Jipii.mp3');
 
     constructor() {
         super().loadImage('img/main/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
+        this.loadImages(this.IMAGES_DEAD);
+        this.loadImages(this.IMAGES_HURT);
         this.applyGravity();
+        this.walking_sound.volume = 0.3;
+        this.jumping_sound.volume = 0.1;
         this.animate();
     }
 
     animate() {
         setInterval(() => {
+            this.walking_sound.pause();
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-                this.x += this.speed;
+                this.moveRight();
                 this.otherDirection = false;
+                this.walking_sound.play();
             }
-
             if (this.world.keyboard.LEFT && this.x > -1000) {
-                this.x -= this.speed;
+                this.moveLeft();
                 this.otherDirection = true;
+                this.walking_sound.play();
+            }
+            if (this.world.keyboard.SPACE && !this.isAboveGround()) {
+                this.jump();
+                this.jumping_sound.play();
             }
             this.world.camera_x = -this.x + 100;
-        } , 1000 / 60);
+        }, 1000 / 60);
 
 
         setInterval(() => {
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                this.playAnimation(this.IMAGES_WALKING);
+            if (this.isDead()) {
+                this.playAnimation(this.IMAGES_DEAD);
+            } else if (this.isHurt()) {
+                this.playAnimation(this.IMAGES_HURT);
+            } else if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.IMAGES_WALKING);
+                }
             }
         }, 50);
     }
 
-    jump() {
-        console.log('jump');
 
-    }
 }
 
 
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,6 +1,6 @@
 class MovableObject {
     x = 120;   // Wie weit!
-    y = 330;    // Wie hoch!
+    y = 320;    // Wie hoch!
     img;       // Bild
     height = 150; // Höhe
     width = 100; // Breite
@@ -10,21 +10,74 @@ class MovableObject {
     otherDirection = false;
     speedY = 0;
     acceleration = 1;
+    energy = 100;
+
+    lastHit = 0;
 
 
     applyGravity() {
         setInterval(() => {
-            if (this.y < 330) {
-            this.y -= this.speedY;
-            this.speedY -= this.acceleration;
-        }
+            if (this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
         }, 1000 / 25);
+    }
+
+    isAboveGround() {
+        return this.y < 278;
     }
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        if (this instanceof Character || this instanceof Chicken) {
+            ctx.beginPath();
+            ctx.lineWidth = '2';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
+    }
+
+    isColliding(mo) {
+        return this.x + this.width > mo.x &&
+            this.y + this.height > mo.y &&
+            this.x < mo.y &&
+            this.y < mo.y + mo.height;
+    }
+
+    hit() {
+        this.energy -= 5;
+        if (this.energy < 0) {
+            this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
+        }
+    }
+
+    isDead() {
+        return this.energy == 0;
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit;
+        timepassed = timepassed / 1000;
+        return timepassed < 1;
+    }
+
+    // isColliding(mo) {
+    //     return (this.x + this.width) >= mo.x && this.x <= (mo.x + mo.width) &&
+    //         (this.y + this.offsetY + this.height) >= mo.y &&
+    //         (this.y + this.offsetY) <= (mo.y + mo.height);
+    // }
 
     loadImages(arr) {
         arr.forEach((path) => {
@@ -35,21 +88,25 @@ class MovableObject {
     }
 
     playAnimation(images) {
-        let i = this.currentImage % this.IMAGES_WALKING.length;
-        let path = this.IMAGES_WALKING[i];
+        let i = this.currentImage % images.length;
+        let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
     }
 
     moveRight() {
-        console.log('move right');
+        this.x += this.speed;
     }
 
     moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000 / 60);
+        this.x -= this.speed;
     }
+
+    jump() {
+        this.speedY = 20;
+    }
+
+    
 }
 
 
